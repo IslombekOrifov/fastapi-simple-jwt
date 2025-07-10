6a5ceaaf2c3c072abe5fd94630e53298b415a69a
Islombek Orifov <islombekorifov0199@gmail.com>
2025-07-11 01:19:17 +0500

    get config class changed, from os variables to dotenv

--- settings.py
+++ settings.py
@@ -1,6 +1,10 @@
 import importlib
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()  
+
 _auth_config = None
 
 def get_auth_config():
@@ -8,7 +12,7 @@ def get_auth_config():
     if _auth_config is None:
         config_path = os.getenv("FASTAPI_AUTH_CONFIG")
         if config_path is None:
-            raise ImportError("Set FASTAPI_AUTH_CONFIG env variable to your AuthConfig path, e.g. 'app.config.AuthConfig'")
+            raise ImportError("Set FASTAPI_AUTH_CONFIG env variable to your Config path, e.g. 'app.config.Config'")
         module_name, class_name = config_path.split('.', 1)
         module = importlib.import_module(module_name)
         _auth_config = getattr(module, class_name)
